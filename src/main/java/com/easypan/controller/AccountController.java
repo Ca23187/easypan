a538d3a35b6b04c64f465b2f6c111a2dc473d32f
Yang <72975943+Ca23187@users.noreply.github.com>
2025-07-07 17:55:41 +0800

    Send verification code.

---
+++ src/main/java/com/easypan/controller/AccountController.java
@@ -0,0 +1,30 @@
+package com.easypan.controller;
+
+import com.easypan.entity.constants.Constants;
+import com.easypan.entity.dto.CreateImageCode;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController("userInfoController")
+public class AccountController {
+
+    @GetMapping("/checkCode")
+    public void checkCode(HttpServletResponse response, HttpSession session, Integer type) throws IOException {
+        CreateImageCode vCode = new CreateImageCode(130, 38, 5, 10);
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
+        response.setContentType("image/jpeg");
+        String code = vCode.getCode();
+        if (type == null || type == 0) {
+            session.setAttribute(Constants.CHECK_CODE_KEY, code);
+        } else {
+            session.setAttribute(Constants.CHECK_CODE_KEY_EMAIL, code);
+        }
+        vCode.write(response.getOutputStream());
+    }
+}
